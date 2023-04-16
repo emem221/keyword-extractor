c59e67828e566000eba40cb53cb271517a90b01e
emem221 <ememikeh@gmail.com>
2023-04-16 16:16:32 +0100

    ui update

--- ai-keyword-extractor/src/App.jsx
+++ ai-keyword-extractor/src/App.jsx
@@ -1,11 +1,14 @@
 import { useState } from "react";
 import { Container, Box } from "@chakra-ui/react";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 const App = () => {
   return (
     <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
       <Container maxW="3x1" centerContent>
-        <h1>my app</h1>
+        <Header />
+        <Footer />
       </Container>
     </Box>
   );
---
+++ ai-keyword-extractor/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import { Box, Text, Image, Flex } from "@chakra-ui/react";
+import logo from "../assets/openai.png";
+
+const Footer = () => {
+  return (
+    <Box marginTop={50}>
+      <Flex justifyContent="center" alignContent="center">
+        <Image src={logo} marginRight={3} />
+        <Text>Powered By Open AI</Text>
+      </Flex>
+    </Box>
+  );
+};
+
+export default Footer;
