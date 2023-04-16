f4c8e709c1384d9940ad35f315f43a9ca57ef54a
emem221 <ememikeh@gmail.com>
2023-04-16 13:09:47 +0100

    first commit

---
+++ ai-keyword-extractor/src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from "react";
+import { Container, Box } from "@chakra-ui/react";
+
+const App = () => {
+  return (
+    <Box bg="blue.600" color="white" height="100vh" paddingTop={130}>
+      <Container maxW="3x1" centerContent>
+        <h1>my app</h1>
+      </Container>
+    </Box>
+  );
+};
+export default App;
